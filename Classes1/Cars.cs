c03e4083ce803707769084f88e6285c06dda778d
DDon71 <theofficial1tap@gmail.com>
2022-09-22 12:38:56 -0400

    First Exercise

---
+++ Classes1/Cars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Classes1
+{
+    public class Cars
+    {
+        public Cars()
+        {
+
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Make: {Make}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {Year}");
+        }
+    }
+}
+
---
+++ Classes1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Classes1;
+using static Classes1.Cars;
+
+
+
+var Car1 = new Cars()
+{
+    Make = "Ferrari",
+    Model = "SF90",
+    Year = "2021"
+};
+
+Car1.PrintCarInfo();
